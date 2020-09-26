925cace74e39c4d57ae5611fa50f96f987288a67
Amit P Naik <amit.naik8103@gmail.com>
2020-09-26 17:21:35 +0530

    test: unit test

---
+++ test/WhiteApp.Test.Unit/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace WhiteApp.Test.Unit
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
